2292886d3fd9748690a6c0901065631bb7041b88
Vivek Verma <vivnps.verma@gmail.com>
2019-01-19 19:22:45 -0800

    Adding Things

--- posts/change_to_p.py
+++ posts/change_to_p.py
@@ -1,5 +1,8 @@
 with open("ml.txt","r") as f:
     text = f.read()
 with open("ml.txt","w") as f:
-    f.write(text.replace("\n","</p><p>"))
+    f.write(text.replace("\n","<p></p>"))
 
+import cv2
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS)
